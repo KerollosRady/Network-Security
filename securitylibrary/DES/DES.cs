c8af954c5118755c31743c7533a3f9a14b254d0b
Kerollos <kokorady23@gmail.com>
2025-03-24 17:42:20 +0200

    initial DES

--- securitylibrary/DES/DES.cs
+++ securitylibrary/DES/DES.cs
@@ -9,8 +9,66 @@ namespace SecurityLibrary.DES
     /// <summary>
     /// If the string starts with 0x.... then it's Hexadecimal not string
     /// </summary>
+
+    // constants:
+
+    // IP, IP-1
+    // 8 s-Boxes each 4*15
+    // Straight Permutation (P)
+    // PC - 1, PC - 2
+
+    // Hexa2bin (String) -> long
+    // bin2Hexa (long) -> String
+    // Permute
+    // Expand
+    // F
+    // Execute
+    // RevertExpansion
+    // GenerateKeys
+
     public class DES : CryptographicTechnique
     {
+        long[] keys = new long[16];
+        long Hexa2bin(String hexa)
+        {
+            long res = 0;
+            return res;
+        }
+        String bin2Hexa(long bin)
+        {
+            String res = "";
+            return res;
+        }
+        long Permute(long bits, ref List<int> p)
+        {
+            long res = 0;
+            return res;
+        }
+        long Expand(long bits)
+        {
+            long res = 0;
+            return res;
+        }
+        long RevertExpansion(long bits)
+        {
+            long res = 0;
+            return res;
+        }
+        long F(long bits, long ki)
+        {
+            long res = 0;
+            return res;
+        }
+        void GenerateKeys(long k)
+        {
+
+        }
+        long Execute(long bits, long k)
+        {
+            GenerateKeys(k);
+            long res = 0;
+            return res;
+        }
         public override string Decrypt(string cipherText, string key)
         {
             throw new NotImplementedException();
