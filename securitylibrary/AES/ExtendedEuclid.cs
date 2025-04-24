3b5bec451ab3bedbc49026b484595cab5f7bdf17
abanoub <abanoubashraf686@gmail.com>
2025-04-24 03:24:19 +0200

    Extended Euclid completed

--- securitylibrary/AES/ExtendedEuclid.cs
+++ securitylibrary/AES/ExtendedEuclid.cs
@@ -14,9 +14,28 @@ namespace SecurityLibrary.AES
         /// <param name="number"></param>
         /// <param name="baseN"></param>
         /// <returns>Mul inverse, -1 if no inv</returns>
+        private int ExtendedGCD(int a, int b, out int x, out int y)
+        {
+            if (b == 0)
+            {
+                x = 1;
+                y = 0;
+                return a;
+            }
+            int gcd = ExtendedGCD(b, a % b, out int x1, out int y1);
+            x = y1;
+            y = x1 - (a / b) * y1;
+            return gcd;
+        }
         public int GetMultiplicativeInverse(int number, int baseN)
         {
-            throw new NotImplementedException();
+            int x, y;
+            int gcd = ExtendedGCD(number, baseN, out x, out y);
+
+            if (gcd != 1)
+                return -1;
+            return (x % baseN + baseN) % baseN;
         }
+
     }
 }
