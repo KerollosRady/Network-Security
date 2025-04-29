bc4670e4ff609f67dac5f1447602c96a36f15271
Kerollos <kokorady23@gmail.com>
2025-04-29 19:02:17 +0300

    RSA Done

--- securitylibrary/RSA/RSA.cs
+++ securitylibrary/RSA/RSA.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SecurityLibrary.AES;
 
 namespace SecurityLibrary.RSA
 {
@@ -10,12 +11,31 @@ namespace SecurityLibrary.RSA
     {
         public int Encrypt(int p, int q, int M, int e)
         {
-            throw new NotImplementedException();
+            int n = p * q;
+            return PowMod(M, e, n);
         }
 
         public int Decrypt(int p, int q, int C, int e)
         {
-            throw new NotImplementedException();
+            int n = p * q;
+            int phi = (p - 1) * (q - 1);
+            int d = new ExtendedEuclid().GetMultiplicativeInverse(e, phi);
+            return PowMod(C, d, n);
+        }
+
+        private int PowMod(long a, long exp, int mod)
+        {
+            long result = 1;
+            a %= mod;
+
+            while (exp > 0)
+            {
+                if ((exp & 1) == 1) result = (result * a) % mod;
+                a = (a * a) % mod;
+                exp >>= 1;
+            }
+
+            return (int)result;
         }
     }
 }
